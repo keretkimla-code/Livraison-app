798245e69b20b457918c1e8f1e9efabad596f41a
keretkimla-code <keretkimla@gmail.com>
2026-08-25 10:45:11 +0000

    Réorganisation en monorepo : client, livreur, backend

--- Livraison-app/LivraisonClient-v0.2-connecte/LivraisonClient/app/src/main/java/com/livraison/client/ui/theme/Color.kt
+++
@@ -1,8 +0,0 @@
-package com.livraison.client.ui.theme
-
-import androidx.compose.ui.graphics.Color
-
-val Orange80 = Color(0xFFFFB74D)
-val Orange40 = Color(0xFFE65100)
-val Green40 = Color(0xFF2E7D32)
-val Neutral95 = Color(0xFFF5F5F5)
